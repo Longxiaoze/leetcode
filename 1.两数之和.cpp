9c8952aa5228724dd07595f6a2ec93b1fca28fda
龙笑泽 <71636559+Longxiaoze@users.noreply.github.com>
2020-09-22 20:45:14 +0800

    Add files via upload

---
+++ 1.两数之和.cpp
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode.cn id=1 lang=cpp
+ *
+ * [1] 两数之和
+ */
+
+// @lc code=start
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        std::unordered_map <int,int> map;
+        for(int i = 0; i < nums.size(); i++) {
+            auto iter = map.find(target - nums[i]);
+            if(iter != map.end()) {
+                return {iter->second, i};
+            }
+            map.insert(pair<int, int>(nums[i], i));
+        }
+        return {};
+    }
+};
+
+// 作者：carlsun-2
+// 链接：https://leetcode-cn.com/problems/two-sum/solution/1-liang-shu-zhi-he-mapzai-ha-xi-fa-zhong-de-jing-d/
+// 来源：力扣（LeetCode）
+// 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+
+// @lc code=end
+
