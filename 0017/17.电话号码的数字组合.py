9dc0952ff0bcf678b6eeb288302d020a14c60c67
龙笑泽 <71636559+Longxiaoze@users.noreply.github.com>
2020-10-08 22:29:42 +0800

    Create 17.电话号码的数字组合.py

---
+++ 0017/17.电话号码的数字组合.py
@@ -0,0 +1,51 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        if not digits:
+            return list()
+        
+        phoneMap = {
+            "2": "abc",
+            "3": "def",
+            "4": "ghi",
+            "5": "jkl",
+            "6": "mno",
+            "7": "pqrs",
+            "8": "tuv",
+            "9": "wxyz",
+        }
+
+        def backtrack(index: int):
+            if index == len(digits):
+                combinations.append("".join(combination))
+            else:
+                digit = digits[index]
+                for letter in phoneMap[digit]:
+                    combination.append(letter)
+                    backtrack(index + 1)
+                    combination.pop()
+
+        combination = list()
+        combinations = list()
+        backtrack(0)
+        return combinations
+ '''  
+ #官方给的精简版，看一下就行了，个人觉得代码还是可读性高一点比较好
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        if not digits:
+            return list()
+        
+        phoneMap = {
+            "2": "abc",
+            "3": "def",
+            "4": "ghi",
+            "5": "jkl",
+            "6": "mno",
+            "7": "pqrs",
+            "8": "tuv",
+            "9": "wxyz",
+        }
+
+        groups = (phoneMap[digit] for digit in digits)
+        return ["".join(combination) for combination in itertools.product(*groups)]
+'''   
